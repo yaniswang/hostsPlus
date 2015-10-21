702e46934bc7220d4ade57fcc7d705af8d4bb495
Yanis Wang <yanis.wang@gmail.com>
2015-10-21 20:06:33 +0800

    修复格式长度问题

--- js/editor.js
+++ js/editor.js
@@ -163,7 +163,7 @@
 			text = text.replace(/^\s*(#!?)?\s*([^\s]+)\s+(.+)/, function(all, comment, ip, others){
 				if(isIp(ip)){
 					var ipLen = /:/.test(ip) ? 41: 16;
-					var space = new Array(ipLen-ip.length).join(' ');
+					var space = new Array(ipLen-ip.length+1).join(' ');
 					return (comment?comment+' ':'') + ip + space + others;
 				}
 				return all;
